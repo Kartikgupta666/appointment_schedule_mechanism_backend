6bab87380dd7f3bbb55630f4e37e175965069e30
kartik Gangil <115064547+Kartikgupta666@users.noreply.github.com>
2024-12-31 13:13:46 +0530

    Update mongo_DB.js

--- Database/mongo_DB.js
+++ Database/mongo_DB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const url = "mongodb+srv://kartikgangil:0123456@cluster0.eqzdo.mongodb.net/"
+const url = "mongodb+srv://kartikgangil:passdalo@cluster0.eqzdo.mongodb.net/"
 function connection() {
     mongoose.connect(url).then(() => {
         console.log("Database connected")
